dea34afad7595dea248c44860225de06f16baaca
jlopesil <jonathan.leoesdejuda@gmail.com>
2022-09-12 23:13:32 -0300

    Initial commit

---
+++ src/main/java/br/com/starwarsapi/ListagemdeFilmes/ListagemdeFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.starwarsapi.ListagemdeFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListagemdeFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListagemdeFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/starwarsapi/ListagemdeFilmes/ListagemdeFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.starwarsapi.ListagemdeFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListagemdeFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
