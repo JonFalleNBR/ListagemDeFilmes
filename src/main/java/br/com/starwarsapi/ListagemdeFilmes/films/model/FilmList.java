56adee86b6c6d7c0cbd4f7a9174c6ab652b9823d
jlopesil <jonathan.leoesdejuda@gmail.com>
2022-09-14 17:27:33 -0300

    Inicio conexão com Banco de Dados

--- src/main/java/br/com/starwarsapi/ListagemdeFilmes/films/model/FilmList.java
+++ src/main/java/br/com/starwarsapi/ListagemdeFilmes/films/model/FilmList.java
@@ -6,11 +6,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class FilmList {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String title;
 
     @JsonProperty("episode_id")
